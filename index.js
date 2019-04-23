8ef360de77c8c2f778bd1b5b9a73ed9fb2ca2fd9
Bruno Guedes <bsguedes@inf.ufrgs.br>
2019-04-23 00:19:08 -0300

    Simple bot

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+
+let args = process.argv.slice(2);
+const port = args[0];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+let prev_dir = -1;
+let direction = -1;
+
+app.get('/players/name', (req, res) => {
+    console.log('got call name');
+    return res.json(
+        {
+            'name': 'nbot'+args[0]
+        }
+    );
+});
+
+app.put('/players/:playerName/move', (req, res) => {
+    let response = '';
+    if (getRandomInt(10) === 0) {
+        prev_dir = -1;
+    }
+    if (prev_dir === -1) {
+        direction = getRandomInt(4);
+    } else {
+        direction = prev_dir;
+    }
+    prev_dir = direction;
+    if (direction === 0){
+        response = 'north';
+    } else if (direction === 1){
+        response = 'east';
+    } else if (direction === 2){
+        response = 'west';
+    } else {
+        response = 'south';
+    }
+    return res.json(
+        {
+            'direction': response
+        }
+    );
+});
+
+app.get('/players/:playerName/radio', (req, res) => {
+    console.log('got call radio');
+    return res.json(
+        {
+            'radio': 0
+        }
+    );
+});
+
+app.post('/players/:playerName/radio', (req, res) => {
+    console.log('got post radio');
+    console.log(req.content);
+    return res.send('nil');
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
